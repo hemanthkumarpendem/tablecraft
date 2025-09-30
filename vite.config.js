f57af5184aa03c2f0a8ec09c9e853720eae4d8c1
Hemanth Kumar Pendem <hemupendem@gmail.com>
2025-09-30 15:46:16 +0530

    Add GitHub Pages config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/tablecraft/',   // 👈 important for GitHub Pages
   plugins: [react()],
 })
